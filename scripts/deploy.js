9bae4155a9f3833aafa0e276e3424e1b81457f01
Juraj Vajda <juraj.vajda@gmail.com>
2025-03-07 22:31:24 -0500

    Copyright year change and some naming to Luanti changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 /**
  * Deploy code to CDB
- * Copyright (C) 2024 SaKeL
+ * Copyright (C) 2025 SaKeL
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
